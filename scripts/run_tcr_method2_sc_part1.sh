7d90ca63155788ae50f87c931e4483bd849464d8
Jerome Samir <jerome.samir@loricahealth.com>
2017-07-06 11:18:09 +1000

    reorded migmap params

--- scripts/run_tcr_method2_sc_part1.sh
+++ scripts/run_tcr_method2_sc_part1.sh
@@ -150,7 +150,7 @@ mkdir -p $param4/summary
 
 for chain in "${CHAIN_ARRAY[@]}"
 do
-	migmap -S $param3 -R ${chain//C} $Q3/${chain}.fa --data-dir=$CONDA_PREFIX/share/igblast $param4/summary/${chain//C}_$param2
+	migmap -S $param3 -R ${chain//C} --data-dir=$CONDA_PREFIX/share/igblast $Q3/${chain}.fa $param4/summary/${chain//C}_$param2
 done
 
 rm -f $CELL_PATH/merged*
