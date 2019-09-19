1359a87930954a881aeb8e033255d74a7c2572d2
Money Gupta <z5168329@infblaa001.(none)>
2019-09-19 10:26:58 +1000

    merge script changes

--- scripts/merge_results.py
+++ scripts/merge_results.py
@@ -80,7 +80,7 @@ for f in files:
                 #find expression
                 expressionfile="kallisto/"+f[0]+"_"+cellID+"/"+f[0]+"_out/abundance.tsv"
                 tpm="0"
-		rpkm="NA"
+		rpkm="0"
                 if os.path.isfile(expressionfile):
                     with open(expressionfile, 'r') as expressionOutput:
                         for line2 in expressionOutput:
